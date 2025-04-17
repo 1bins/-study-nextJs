a9d86b5ed10b51bdc042adbec64e69f068943dff
서원빈 <1bins@kakao.com>
2025-04-17 10:03:01 +0900

    :sparkles: 페이지 라우팅 설정, 네비게이팅

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    const onClickButton = () => {
+        router.push("/test");
+        // replace: 뒤로가기 방지하면서 이동
+        // back: 뒤로가기
+    };
+
   return (
       <>
-        <header>글로벌 헤더</header>
+        <header>
+            <Link href={"/"}>index</Link>
+            &nbsp;
+            <Link href={"/search"}>search</Link>
+            &nbsp;
+            <Link href={"/book/1"}>book/1</Link>
+            <div>
+                <button onClick={onClickButton}>/test 페이지로 이동</button>
+            </div>
+        </header>
         <Component {...pageProps} />
       </>
   )
