8fd9d520e489f7edf86aa7832095ea762dee04aa
서원빈 <1bins@kakao.com>
2025-04-17 10:32:36 +0900

    :sparkles: 프리페칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
@@ -10,13 +11,24 @@ export default function App({ Component, pageProps }: AppProps) {
         // replace: 뒤로가기 방지하면서 이동
         // back: 뒤로가기
     };
+    
+    /**
+     * prefetch
+     * => 현재 페이지에 연결된 페이지들의 JS번들링을 미리 요청함
+     * */
+    useEffect(() => {
+      // onClickButton에 프로그래머틱하게 이동을 구현했기 때문에 pre-fetch를 직접적으로 구현해준다
+      router.prefetch("/test");
+
+      // Link태그에 prefetch={false}를 부여하여 해제할 수도 있다.
+    }, []);
 
   return (
       <>
         <header>
             <Link href={"/"}>index</Link>
             &nbsp;
-            <Link href={"/search"}>search</Link>
+            <Link href={"/search"} prefetch={false}>search</Link>
             &nbsp;
             <Link href={"/book/1"}>book/1</Link>
             <div>
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 export default function Page() {
     const router = useRouter();
     const { q } = router.query;
+    console.log(q)
 
     return <h1>Search</h1>;
 }
