7a912c736073d98abe9169500898d9a5c32d89bc
서원빈 <1bins@kakao.com>
2025-06-03 21:18:26 +0900

    리뷰 재검증 (revalidatePath)

--- section05/src/actions/create-review.action.ts
+++ section05/src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import {revalidatePath} from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
   const content = formData.get('content')?.toString();
@@ -19,6 +21,7 @@ export async function createReviewAction(formData: FormData) {
       })
     });
     console.log(response.status);
+    revalidatePath(`/book/${bookId}`); // path로 전달한 페이지의 캐시를 전부 비워버리고 다시 가져옴 => force-cache 무시, 서버 컴포넌트에서만 호출할 수 있음
   } catch(err){
     console.error(err);
     return;
