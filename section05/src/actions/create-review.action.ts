15e84525d2cfb584b1875e1f74ed07e06774574c
서원빈 <1bins@kakao.com>
2025-06-04 21:37:23 +0900

    다양한 재검증 방식

--- section05/src/actions/create-review.action.ts
+++ section05/src/actions/create-review.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import {revalidatePath} from "next/cache";
+import {revalidateTag} from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
@@ -21,7 +21,22 @@ export async function createReviewAction(formData: FormData) {
       })
     });
     console.log(response.status);
-    revalidatePath(`/book/${bookId}`); // path로 전달한 페이지의 캐시를 전부 비워버리고 다시 가져옴 => force-cache 무시, 서버 컴포넌트에서만 호출할 수 있음
+    // path로 전달한 페이지의 캐시를 전부 비워버리고 다시 가져옴 => force-cache 무시, 서버 컴포넌트에서만 호출할 수 있음
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // 2. 특정 경로의 모든 동적 페이지를 재검증
+    // revalidatePath('book/[id]', 'page');
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath('/(with-searchbar)', 'layout');
+
+    // 4. 모든 데이터 재검증
+    // revalidatePath('/', 'layout');
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidateTag("review-${bookId}");
+
   } catch(err){
     console.error(err);
     return;
